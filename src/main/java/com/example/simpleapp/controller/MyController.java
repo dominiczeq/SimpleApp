8f6ac7dbc4b5fd095c63767c608f0b2f961b7bd5
Dawid Tyrała <33028235+dominiczeq@users.noreply.github.com>
2019-02-11 16:16:32 +0100

    Add simle method to create a new User in very primitive way (for test)

--- src/main/java/com/example/simpleapp/controller/MyController.java
+++ src/main/java/com/example/simpleapp/controller/MyController.java
@@ -1,18 +1,41 @@
 package com.example.simpleapp.controller;
 
+import com.example.simpleapp.entity.User;
+import com.example.simpleapp.repository.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.annotation.PostConstruct;
 
 @Controller
 public class MyController {
-    @RequestMapping("/home")
+
+    @Autowired
+    private UserRepo userRepo;
+
+    @RequestMapping({"/", "/home"})
     public ModelAndView home() {
         ModelAndView model = new ModelAndView("home");
         String name = "Dawid";
         model.addObject("name", name);
         return model;
     }
+
+//    @PostConstruct
+//    public String CreateUser () {
+//        User u = new User();
+//        u.setUsername("admin");
+//        u.setUserRole("ROLE_ADMIN");
+//        u.setPassword("admin");
+//        u.setEnabled(true);
+//        userRepo.save(u);
+//
+//        return "/person/list";
+//
+//    }
 
 }
