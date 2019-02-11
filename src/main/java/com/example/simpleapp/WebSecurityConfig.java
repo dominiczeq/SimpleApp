c0567b272f934a347f631a10f69123039ced5db1
Dawid Tyrała <33028235+dominiczeq@users.noreply.github.com>
2019-02-11 16:11:18 +0100

    Add method authentication users saved in database, created User entity and UserRepo in previous commit

--- src/main/java/com/example/simpleapp/WebSecurityConfig.java
+++ src/main/java/com/example/simpleapp/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.simpleapp;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -8,6 +9,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import javax.sql.DataSource;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -22,16 +24,27 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .formLogin();
 
     }
+    
+    //Get users from database source
+    @Autowired
+    DataSource dataSource;
 
-    @Override
-    public void configure(AuthenticationManagerBuilder auth) throws Exception {
-        BCryptPasswordEncoder encoder = passwordEncoder();
-        auth
-                .inMemoryAuthentication()
-                .withUser("admin").password(encoder.encode("admin")).roles("ADMIN")
-                .and()
-                .withUser("user").password(encoder.encode("password")).roles("USER");
+    @Autowired
+    public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication().dataSource(dataSource)
+                .passwordEncoder(passwordEncoder())
+                .usersByUsernameQuery("SELECT username, password, enabled  FROM `simple-app`.user where username=?")
+                .authoritiesByUsernameQuery("select username, user_role from `simple-app`.user where username=?");
     }
+//    @Override
+//    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        BCryptPasswordEncoder encoder = passwordEncoder();
+//        auth
+//                .inMemoryAuthentication()
+//                .withUser("admin").password(encoder.encode("admin")).roles("ADMIN")
+//                .and()
+//                .withUser("user").password(encoder.encode("password")).roles("USER");
+//    }
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
